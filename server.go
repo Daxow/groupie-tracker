91db0f5f1c8b41dee575f21df44c8b237de17fdb
Daxow <voxizofficiel@gmail.com>
2026-01-19 11:49:45 +0100

    change style.css

--- server.go
+++ server.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"fmt"
+	"strings"
 )
 
 type Server struct {
@@ -24,6 +26,8 @@ func NewServer() *Server {
 
 func (s *Server) RegisterRoutes() {
 	http.HandleFunc("/", s.handleHome)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 }
 
 func (s *Server) handleHome(w http.ResponseWriter, r *http.Request) {
@@ -41,4 +45,55 @@ func (s *Server) renderPage(w http.ResponseWriter, name string, data []Artist) {
 		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
 		log.Println("Erreur template :", err)
 	}
+}
+
+func SearchArtists(artists []Artist, search string) []Artist {
+	var result []Artist
+
+	search = strings.ToLower(search)
+
+	for _, artist := range artists {
+		found := false
+
+		if strings.Contains(strings.ToLower(artist.Name), search) {
+			found = true
+		}
+
+		if !found {
+			for _, member := range artist.Members {
+				if strings.Contains(strings.ToLower(member), search) {
+					found = true
+					break
+				}
+			}
+		}
+
+		if !found {
+			creation := fmt.Sprintf("%d", artist.CreationDate)
+			if strings.Contains(creation, search) {
+				found = true
+			}
+		}
+
+		if !found {
+			if strings.Contains(strings.ToLower(artist.FirstAlbum), search) {
+				found = true
+			}
+		}
+
+		if !found {
+			for _, location := range artist.Location {
+				if strings.Contains(strings.ToLower(location), search) {
+					found = true
+					break
+				}
+			}
+		}
+
+		if found {
+			result = append(result, artist)
+		}
+	}
+
+	return result
 }
--- structs.go
+++ structs.go
@@ -7,4 +7,20 @@ type Artist struct {
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
+	Location	 []string `json:"-"`
+}
+
+type Location struct {
+	ID		 int      `json:"id"`
+	Location []string `json:"location"`
+}
+
+type Date struct {
+	ID    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type Relations struct {
+	ID        int              `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
 }
