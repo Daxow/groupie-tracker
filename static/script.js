a60431e2b908ed7468be154773a9cc3268fb9e7a
Daxow <voxizofficiel@gmail.com>
2026-02-12 12:58:42 +0100

    add searchbar input suggestions

--- static/script.js
+++ static/script.js
@@ -6,3 +6,34 @@ fetch("/api/artists")
         allArtists = data
         console.log(allArtists)
     })
+
+const searchInput = document.getElementById("searchInput")
+const suggestionsContainer = document.getElementById("suggestions")
+
+searchInput.addEventListener("input", function () {
+
+    const searchValue = searchInput.value.toLowerCase()
+
+    suggestionsContainer.innerHTML = ""
+
+    if (searchValue.length === 0) {
+        return
+    }
+
+    const filteredArtists = allArtists.filter(function (artist) {
+        return artist.name.toLowerCase().includes(searchValue)
+    })
+
+    filteredArtists.forEach(function (artist) {
+        const suggestionElement = document.createElement("div")
+        suggestionElement.textContent = artist.name
+
+    suggestionElement.addEventListener("click", function () {
+        searchInput.value = artist.name
+        suggestionsContainer.innerHTML = ""
+    })
+
+    suggestionsContainer.appendChild(suggestionElement)
+    })
+
+})
